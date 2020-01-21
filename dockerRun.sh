d65d9dd62394d9f852a65f626f6731ea3e023de4
Mike George Verros <mixaverros88@users.noreply.github.com>
2020-01-21 14:50:17 +0200

    add comments for finding the docker machine ip

--- dockerRun.sh
+++ dockerRun.sh
@@ -6,3 +6,7 @@ cd deploy
 curl -LOk https://github.com/mixaverros88/java-api/raw/master/target/demorest.war
 # run the docker
 winpty docker run -it -p 80:8080 -v /$(pwd):/usr/local/tomcat/webapps mixaverross88/java-restfull-api:1.0
+# you can reach this service through docker machine ip, also add the demorest folder to the path
+# to find docker-machine ip execute the following command
+# docker-machine ls
+# example URL http://192.168.99.100/demorest/
